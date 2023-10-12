eed9bc2d38c63070df7cc4c9e3bfae4a772073e8
Sakrit-Singh <147722783+Sakrit-Singh@users.noreply.github.com>
2023-10-12 18:10:49 +0530

    Add files via upload

---
+++ CahtBOT.py
@@ -0,0 +1,99 @@
+print("HI!".center(150))
+print("What is your good name".center(150))
+print("---------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+name= input()
+print("HI!",name)
+print("I Have three levels for you",name)
+s =("Science QUIZ","Tech TALK","SK MOTORS")
+for z in s:
+    print(z)
+print("What is is the change in colour of Red litmus paper when it is mixed with a base")
+v2=input()
+if v2== "Blue":  
+    print("Correct Goooooooooooooooooood!")
+else:   
+    print("Wrong the answer is Blue")
+print("Q2- Who discovered Nitrogen?")
+v3=input()       
+if v3=="Rutherford":
+    print("Correct Very Good")
+else:
+    print("Wrong the answer is Rutherford")
+print("Q3- What is the Scientific name of algea")
+q3=input()
+if q3=="Spirogaira":
+    print("Correct")
+else: 
+    print("No, The answer is Spirogaira")
+print("Level TWO".center(150))
+print("Do you know Twitter has been Purchased by Elon musk?????")
+v5=input()
+print("Now you tell me a fact which i dont know")
+v6=input()
+print("I don't know it")
+print("Do you know how many hairs Bill Gates have")
+print("Enter yes if you Know")
+v6=input().lower
+print("now let me tell you he has -0000 hairs!")
+print("Hello".center(150))
+print("Welcome to SK Motors".center(150))
+print("Here You will Get A vast varity of Cars".center(150))
+print("Enter the name of the car which you want to purchase")
+cars = ("1.Supra","2.Mercedes","3.BMW","4.Lamborghini","5.Audi","6.Porshe","7.Rolls Royce","8.Nissan GTR","9.Ford Mustang","10.Ferrari")
+for X in cars:
+    print(X)
+v1=input()
+if v1=="Supra":
+    print("Price is=> Rs. 85.00 Lakh")
+    print("Have Fun RATATATATATATATATATATA!")
+    print("Pay us and we will deliver your car")
+    print("Thank you")
+elif v1=="Mercedes":
+    print("The price is=> 2.17cr")
+    print("The price is=>5.38cr")
+    print("Have Fun in Driving It")
+    print("Pay us and we will deliver your car")
+    print("Thank you")
+elif v1=="Ferrari":
+    print("the price is=> 12.97cr")
+    print("Have Fun in driving it")
+    print("Pay us and we will deliver your car")
+    print("Thank you")
+elif v1=="BMW":
+    print("The price is=> 4.23 cr")
+    print("Have fun in Driving it")
+    print("Pay us and we will deliver your car")
+    print("Thank you")
+elif v1=="Lamborghini":
+    print("The  price is => 21.16 cr")
+    print("Have fun in Driving it")
+    print("Pay us and we will deliver your car")
+    print("Thank you")
+elif v1=="Audi":
+    print("The price is => 49 lakhs")
+    print("Have Fun in Driving it")
+    print("Pay us and we will deliver your car")
+    print("Thank you")
+elif v1=="Porshe":
+    print("The Price is=> 11.54 cr")
+    print("Have fun in driving it")
+    print("Pay us and we will deliver your car")
+    print("Thank you")
+elif v1=="Rolls Royce":
+    print("The price is=> 7.43 cr")
+    print("have fun in driving it")
+    print("Pay us and we will deliver your car")
+    print("Thank you")
+elif v1=="Nissan GTR":
+    print("The price is=> 2.2 cr")
+    print("Have fun in Driving it")
+    print("Pay us and we will deliver your car")
+    print("Thank you")
+elif v1=="Ford Mustang GT":
+    print("The price is=> 76.61 Lakhs")
+    print("Have Fun in Driving it")
+    print("Pay us and we will deliver your car")
+    print("Thank you")
+else:
+    print("We do not have That car")
+    print("Thank you")
